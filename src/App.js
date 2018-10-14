4ac2acbab07784373c0c41be100c9b3beabd03c9
Andrew <chicken.noodlesoup99@gmail.com>
2018-10-14 14:56:51 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      name: '',
+      picture: ''
+    }
+  }
+
+  componentDidMount() {
+    // Axios Request
+    // axios.get('http://pokeapi.salestock.net/api/v2/pokemon/65')
+    //      .then(response => {
+    //        console.log('response.data', response.data)
+    //        this.setState({
+    //          name: response.data.name,
+    //          picture: response.data.sprites.front_default,
+    //        })
+    //      })
+    //      .catch(error => console.log('Axios Error GET', error))
+
+    // Fetch 
+    fetch('http://pokeapi.salestock.net/api/v2/pokemon/428', {
+      // second parameter is an init object that allows you to control different settings
+      method: "GET", 
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    })
+      .then(response => {
+        // Checking to see if response.ok is true
+        if (response.ok) {
+          return response.json();
+        }
+        // If response was not ok, throw an error
+        throw new Error('Network response was not ok')
+      })
+      .then(data => {
+        // If want the entire data back as a string, can do JSON.stringify()
+        console.log('JSON.stringify', JSON.stringify(data))
+        console.log(typeof JSON.stringify(data)) // data is a string
+        this.setState({
+          name: data.name,
+          picture: data.sprites.front_default,
+        })
+      })
+      .catch(error => console.log('Fetch error:', error.message))
+   
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       Promise Practice
+       <h1>Pokemon</h1>
+       <h3>Name: {this.state.name}</h3>
+        {this.state.picture ? <img src={this.state.picture} alt="pokemon"/> : ''}
       </div>
     );
   }
